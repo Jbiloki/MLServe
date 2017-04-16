46bff70dddf81946d9606cf76e165ea937bddbda
Jacob <Bilokij@gmail.com>
2017-04-16 08:58:04 -0700

    Added web call to data set

--- app/src/main/java/com/example/nguyen/mlpost/UCIExplainFrag.java
+++ app/src/main/java/com/example/nguyen/mlpost/UCIExplainFrag.java
@@ -1,16 +1,28 @@
 package com.example.nguyen.mlpost;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class UCIExplainFrag extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         final View mView = inflater.inflate(R.layout.activity_uciexplain_frag, container,false);
+        TextView dataSet = (TextView)mView.findViewById(R.id.Data);
+        dataSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent web = new Intent(Intent.ACTION_VIEW);
+                web.setData(Uri.parse("http://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29"));
+                startActivity(web);
+            }
+        });
         return mView;
     }
 }
